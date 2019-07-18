e0785bd90cee5b8e68a2dcbe3ee75921627a8b84
Vladimir Chernikov <vladimir.v.chernikov@gmail.com>
2019-07-18 12:55:18 +0300

    hw03_easy done

--- hw_03/hw03_easy.py
+++ hw_03/hw03_easy.py
@@ -5,8 +5,17 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
-
+    base = 10 ** ndigits
+    retVal = number * base
+    decimals = retVal % 1
+    if decimals <= 0.5:
+        decimals = 0
+    else:
+        decimals = 1
+    retVal //= 1
+    retVal += decimals
+    retVal /= base
+    return retVal
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
@@ -20,9 +29,19 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
-
+    retVal = True
+    digits = []
+    while (ticket_number > 0):
+        digits.append(ticket_number % 10)
+        ticket_number //= 10
+    digitsCnt = len(digits)
+    middle = digitsCnt // 2
+    if digitsCnt % 2 == 0:
+        retVal = sum(digits[0: middle]) == sum(digits[middle: digitsCnt])
+    else:
+        retVal = sum(digits[0: middle]) == sum(digits[middle + 1: digitsCnt])
+    return retVal
 
 print(lucky_ticket(123006))
-print(lucky_ticket(12321))
+print(lucky_ticket(12322))
 print(lucky_ticket(436751))
