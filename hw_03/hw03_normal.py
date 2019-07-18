97b992f1804300aa2c05d92970c9666d6e34729e
Vladimir Chernikov <vladimir.v.chernikov@gmail.com>
2019-07-18 13:34:14 +0300

    hw03_normal

--- hw_03/hw03_normal.py
+++ hw_03/hw03_normal.py
@@ -15,18 +15,42 @@ print(fibonacci(7, 10))
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
-
 def sort_to_max(origin_list):
-    pass
+    for i in range(0, len(origin_list)):
+        for j in range(0, i):
+            if origin_list[i] < origin_list[j]:
+                k = origin_list[i]
+                origin_list[i] = origin_list[j]
+                origin_list[j] = k
+    return origin_list
 
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+def my_filter(func, my_list):
+    retVal = []
+    for elem in my_list:
+        if func(elem):
+            retVal.append(elem)
+    return retVal
+
+l1 = [1, 17, -2, 3, 13, 4, 23]
+print(my_filter(lambda x : x > 10, l1))
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+def isParallelogram(A1, A2, A3, A4):
+    retVal = False
+
+    return retVal
+
+A1 = (1, 3)
+A2 = (4, 7)
+A3 = (2, 8)
+A4 = (-1, 4)
+print(isParallelogram(A1, A2, A3, A4))
